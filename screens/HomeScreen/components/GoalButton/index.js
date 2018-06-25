dc567633c2374bea8ad21c978f611f99b216a947
Zeljko Tadic <zeljko.tadic.91@gmail.com>
2018-06-25 14:49:20 +0200

    add initial home screen button

---
+++ screens/HomeScreen/components/GoalButton/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, TouchableHighlight, Text, View } from 'react-native';
+
+export default ({ onClick, title, subTitle }) => {
+  return (
+    <TouchableHighlight onPress={this._onPressButton}>
+      <View
+        accessibilityLabel="Learn more about this purple button"
+        style={styles.container}
+      >
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subTitle}>{subTitle}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    marginHorizontal: 20,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#d6d7da',
+    marginBottom: 20,
+    paddingVertical: 15,
+    paddingHorizontal: 20
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 5,
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    fontSize: 16,
+  }
+});
--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {GoalButton} from './components';
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Home screen</Text>
+      <View style={styles.container}>    
+        <View>
+          <Image
+            style={{width: 44, height: 22}}
+          />
+          <Text>Welcome to 8fit</Text>
+        </View>
+        <Text>What's your goal</Text>
+        <GoalButton title={'Lose weight'} subTitle={'Burn fat & get lean'}/>
+        <GoalButton title={'Get fitter'} subTitle={'Tone up & feel healthy'}/>
+        <GoalButton title={'Gain muscle'} subTitle={'Build mass & strength'}/>
       </View>
     );
   }
@@ -17,6 +27,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
   },
 });
 
