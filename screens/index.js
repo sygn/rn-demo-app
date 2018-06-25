45026a4d38914227817b9f9c2b038770e66f0587
Zeljko Tadic <zeljko.tadic.91@gmail.com>
2018-06-25 14:49:28 +0200

    export screens

---
+++ screens/index.js
@@ -0,0 +1,4 @@
+export {default as HomeScreen} from './HomeScreen';
+export {default as AgeScreen} from './AgeScreen';
+export {default as HeightScreen} from './HeightScreen';
+export {default as SummaryScreen} from './SummaryScreen';
