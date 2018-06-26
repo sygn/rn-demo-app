951c907750f015c30a9144e7927be6d59bb41c4d
Zeljko Tadic <zeljko.tadic.91@gmail.com>
2018-06-26 11:11:58 +0200

    header container

--- shared/Header/index.js
+++ shared/Header/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
-export default ({title}) => {
+const Header = ({title}) => {
   return (
-    <Text style={styles.header}>{title}</Text>
+    <View style={styles.headerContainer}>
+      <Text
+        style={styles.header}
+      >
+        {title}
+      </Text>
+    </View>
   );
 }
+
+Header.defaultProps = {
+  title: '',
+};
+
+export default Header;
 
 const styles = StyleSheet.create({
   header :{
     fontSize: 20,
     fontWeight: 'bold',
     marginTop: 26,
+    textAlign: 'center',
   }
 });  
